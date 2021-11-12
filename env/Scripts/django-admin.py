ea1efbb8a1616edd3ab4bb38643f5d3f755a18a8
Slimjim821 <jim.n.johnson16@gmail.com>
2021-11-12 10:50:32 -0600

    test

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jimnj\onedrive\desktop\cisba\cidm6325_ecomm\django_blog\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
